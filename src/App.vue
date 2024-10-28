122272f2b7c5190ca805cedff1a1b92d8c5e3310
zzx <1@1.com>
2024-10-28 14:35:29 +0800

    zzx

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 import PersonalPanel from '@/views/Core/PersonalPanel.vue';
 import NoticePanel from './views/Core/NoticePanel.vue';
+import MessagePanel from './views/Core/MessagePanel.vue';
 </script>
 
 <template>
   <div id="app">
-    <router-view></router-view>
-     <!-- <personal-panel></personal-panel> -->
-      <!-- <notice-panel></notice-panel> -->
+    <!-- <router-view></router-view> -->
+    <!-- <personal-panel></personal-panel> -->
+    <!-- <notice-panel></notice-panel> -->
+    <message-panel></message-panel>
   </div>
 </template>
 
--- src/views/Core/MessagePanel.vue
+++ src/views/Core/MessagePanel.vue
@@ -1,10 +1,27 @@
 <template>
-    <div>
-
+    <div class="message-panel">
+        <div class="message-header">您有 {{ data.length }} 条消息</div>
+        <div class="message-content">
+            <div class="message-item" v-for="item in data" :key="item.key">
+                <img :src="item.avatar" alt="" class="avatar">
+                <span class="sender">
+                    {{ item.sender }}
+                </span>
+                <span class="time">
+                    <li class="fa fa-clock-o"></li> {{ item.time }}
+                </span>
+                <div class="message-content">
+                    {{ item.content }}
+                </div>
+            </div>
+        </div>
+        <div class="message-footer">查看所有消息</div>
     </div>
 </template>
 
 <script setup>
+
+import userAvatar from "@/assets/user.png"
 
 const props = defineProps({
     data: {
@@ -12,35 +29,35 @@ const props = defineProps({
         default: () => [
             {
                 key: "1",
-                avatar: '@/assets/user.png',
+                avatar: new URL('@/assets/user.png', import.meta.url).href,
                 content: '你修改了用户密码',
                 sender: '诸葛亮',
                 time: '5分钟前'
             },
             {
                 key: "2",
-                avatar: '@/assets/user.png',
+                avatar: new URL('@/assets/user.png', import.meta.url).href,
                 content: '你修改了用户头像',
                 sender: '武则天',
                 time: '2小时前'
             },
             {
                 key: "3",
-                avatar: '@/assets/user.png',
+                avatar: new URL('@/assets/user.png', import.meta.url).href,
                 content: '今日25名新成员加入',
                 sender: '王语嫣',
                 time: '昨天'
             },
             {
                 key: "4",
-                avatar: '@/assets/user.png',
+                avatar: new URL('@/assets/user.png', import.meta.url).href,
                 content: '您发表了一篇新随笔',
                 sender: '刘备',
                 time: '昨天'
             },
             {
                 key: "5",
-                avatar: '@/assets/user.png',
+                avatar: new URL('@/assets/user.png', import.meta.url).href,
                 content: '您发表了一篇新随笔',
                 sender: '上官婉儿',
                 time: '前天'
@@ -56,4 +73,64 @@ function handleClick() {
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.message-panel {
+    font-size: 15px;
+    width: 300px;
+    margin: -12px;
+
+    .message-header {
+        padding-left: 10px;
+        font-size: 14px;
+        padding-top: 6px;
+        padding-bottom: 6px;
+    }
+
+    .message-content {
+        font-size: 15px;
+        .message-item {
+            border-color: rgba(180, 190, 190, 0.8);
+            border-top-width: 1px;
+            border-top-style: solid;
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+        .message-item:hover {
+            cursor: pointer;
+            background: #b1a6a61e;
+        }
+        .message-avatar {
+            padding-left: 10px;
+        }
+        .avatar {
+            margin-right: 10px;
+        }
+        .sender {
+            font-size: 16px;
+            font-weight: bold;
+        }
+        .time {
+            font-size: 12px;
+            float: right;
+            padding-left: 10px;
+        }
+    }
+    .message-footer {
+        font-size: 14px;
+        text-align: center;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        border: rgba(180, 190, 190, 0.8) 1px solid;
+    }
+    .message-footer:hover {
+        cursor: pointer;
+        background: #b1a6a61e;
+    }
+    .avatar {
+        width: 40px;
+        height: 40px;
+        border-radius: 20px;
+        float: left;
+    }
+}
+</style>
